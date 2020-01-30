3e7d0fd1c9cc40504e51511e6eb8ce509fe7ae26
ana.garcia <ana.garcia@castinformatica.onmicrosoft.com>
2020-01-30 13:57:11 -0300

    Implementa lazy loading no projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+		path: '',
+		loadChildren: 'src/app/components/login/login.module#loginModule'
+	},
+  {
+		path: 'login',
+		loadChildren: 'src/app/components/login/login.module#loginModule'
+	}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
