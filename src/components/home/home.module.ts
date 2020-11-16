082d505c85738ced917e2c52ad3a3635aab0f149
ana.garcia <ana.bya.tvd@gmail.com>
2020-11-16 14:31:46 -0300

    Adiciona exemplo de importação da modal de confimação

--- src/components/home/home.module.ts
+++ src/components/home/home.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from "@angular/common";
 
 import { homeRoutingModule } from "./home-routing.module";
 import { HomeComponent } from "./home.component";
+import { ModalConfimacaoComponent } from "../modal-confimacao/modal-confimacao.component";
 
 @NgModule({
   imports: [CommonModule, homeRoutingModule],
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, ModalConfimacaoComponent],
 })
 export class homeModule {}
---
+++ src/components/modal-confimacao/modal-confimacao.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-modal-confimacao",
+  templateUrl: "./modal-confimacao.component.html",
+  styleUrls: ["./modal-confimacao.component.scss"],
+})
+export class ModalConfimacaoComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
