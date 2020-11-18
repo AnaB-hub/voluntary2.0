782550f5ca7341fc5ecd04ce4e5c4d3f78a7a3cc
ana.garcia <ana.bya.tvd@gmail.com>
2020-11-18 14:47:35 -0300

    Adiciona design inicial da tela de login

--- src/components/login/login.component.ts
+++ src/components/login/login.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"],
 })
 export class LoginComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
+    this.inicializarFormulario();
   }
 
+  inicializarFormulario(): void {
+    this.form = this.formBuilder.group({
+      user: [null, Validators.required],
+      senha: [true, Validators.required],
+    });
+  }
 }
